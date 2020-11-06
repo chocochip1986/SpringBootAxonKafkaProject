b64c1f5ad36d3d7b921aafb1f39dbce69b51b8d4
Ou Guozheng <abc@abc.com>
2020-11-06 14:05:00 +0800

    managed to get the application up and running

--- src/main/java/axon/config/KafkaConfig.java
+++ src/main/java/axon/config/KafkaConfig.java
@@ -9,22 +9,46 @@ import org.axonframework.extensions.kafka.configuration.KafkaMessageSourceConfig
 import org.axonframework.extensions.kafka.eventhandling.DefaultKafkaMessageConverter;
 import org.axonframework.extensions.kafka.eventhandling.KafkaMessageConverter;
 import org.axonframework.extensions.kafka.eventhandling.consumer.ConsumerFactory;
+import org.axonframework.extensions.kafka.eventhandling.consumer.DefaultConsumerFactory;
 import org.axonframework.extensions.kafka.eventhandling.consumer.Fetcher;
 import org.axonframework.extensions.kafka.eventhandling.consumer.subscribable.SubscribableKafkaMessageSource;
 import org.axonframework.extensions.kafka.eventhandling.producer.ConfirmationMode;
 import org.axonframework.extensions.kafka.eventhandling.producer.DefaultProducerFactory;
 import org.axonframework.extensions.kafka.eventhandling.producer.ProducerFactory;
 import org.axonframework.serialization.json.JacksonSerializer;
-import org.axonframework.serialization.xml.XStreamSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 @Configuration
 public class KafkaConfig {
+    @Value(value = "${axon.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${axon.kafka.client-id}")
+    private String clientId;
+
+    @Value(value = "${axon.kafka.default-topic}")
+    private String defaultTopic;
+
+    @Bean
+    @Primary
+    public KafkaProperties kafkaProperties() {
+        KafkaProperties kafkaProperties = new KafkaProperties();
+        kafkaProperties.setBootstrapServers(
+                new ArrayList<>(){{
+                    add(bootstrapAddress);
+                }});
+        kafkaProperties.setClientId(clientId);
+        kafkaProperties.setDefaultTopic(defaultTopic);
+        return kafkaProperties;
+    }
+
     //CONSUMER RELATED BEANS
     @Bean
     public KafkaMessageSourceConfigurer kafkaMessageSourceConfigurer() {
@@ -40,6 +64,11 @@ public class KafkaConfig {
     @Bean
     public KafkaMessageConverter<String, byte[]> kafkaMessageConverter() {
         return DefaultKafkaMessageConverter.builder().serializer(JacksonSerializer.builder().build()).build();
+    }
+
+    @Bean
+    public ConsumerFactory<String, byte[]> consumerAxonFactory(KafkaProperties kafkaProperties) {
+        return new DefaultConsumerFactory<>(kafkaProperties.buildConsumerProperties());
     }
 
     @Bean
@@ -69,8 +98,8 @@ public class KafkaConfig {
 
     //PRODUCER RELATED BEANS
     @Bean
-    public ProducerFactory<String, ByteArray> producerAxonKafkaFactory(KafkaProperties kafkaProperties) {
-        return new DefaultProducerFactory.Builder<String, ByteArray>()
+    public ProducerFactory<String, byte[]> producerAxonKafkaFactory(KafkaProperties kafkaProperties) {
+        return new DefaultProducerFactory.Builder<String, byte[]>()
                 .configuration(kafkaProperties.buildProducerProperties())
                 .producerCacheSize(10000)
                 .confirmationMode(ConfirmationMode.WAIT_FOR_ACK)
